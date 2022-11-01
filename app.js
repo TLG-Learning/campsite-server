9eb907ee98faf7e1470528fd955cddd49c9d0331
Donte Burney <donteburney@Dontes-Mac-mini.local>
2022-11-01 10:52:04 -1000

    updated

---
+++ app.js
@@ -0,0 +1,27 @@
+require("dotenv").load();
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const axios = require("axios");
+const { stringify } = require("flatted");
+
+app.use(cors());
+
+let config = {
+  headers: {
+    apiKey: process.env.API_KEY,
+  },
+};
+
+//UFC API Starts Here
+app.get("/api/campsites", async function (req, res) {
+  const results = await axios.get(
+    "https://ridb.recreation.gov/api/v1/facilities?limit=50&offset=0&state=CO&lastupdated=10-01-2018",
+    config
+  );
+
+  res.send(stringify(results));
+});
+
+app.listen(process.env.PORT || 3000);
